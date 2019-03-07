b421bea20f2d424b6bf8be2b1cba46b89a01a025
some cunt who flunked C <afonso_rosa@hotmail.com>
2019-03-07 17:17:42 +0000

    Argumentos

---
+++ Argumentos/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Argumentos
+{
+    class Program
+    { /// <summary>
+      /// Aqui começa o código.
+      /// </summary>
+      /// <param name="args"> Lê os argumentos </param>
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine(args[i]);
+            }
+        }
+    }
+}
